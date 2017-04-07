466711e2dee6b299a3346a0af1829d8cc121402e
castro150 <rafaelcarneiroget@hotmail.com>
2017-04-07 13:58:41 -0300

    Incrementando Customer

--- models/Customer.js
+++ models/Customer.js
@@ -6,6 +6,20 @@ var CustomerSchema = new mongoose.Schema({
   number: Number,
   name: String,
   status: String,
+  type: String,
+  cpf: String,
+  address: String,
+  district: String,
+  city: String,
+  state: String,
+  cep: String,
+  email: String,
+  contacts: [{ name: String, tel: String, cel: String }],
+  cei: String,
+  gpsCode: String,
+  accessCode: String,
+  accessPassword: String,
+  obs: String,
   accessoryObligations: [{ name: String, activationDate: Date }]
 });
 
