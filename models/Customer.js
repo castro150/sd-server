07e66d6f711d2fe892c55a7fc968238a6c9803d2
castro150 <rafaelcarneiroget@hotmail.com>
2017-04-10 14:20:49 -0300

    Adicionando campos e formatando

--- models/Customer.js
+++ models/Customer.js
@@ -3,26 +3,37 @@
 var mongoose = require('mongoose');
 
 var CustomerSchema = new mongoose.Schema({
-  number: Number,
-  alterNumber: Number,
-  name: String,
-  status: String,
-  type: String,
-  cpf: String,
-  startServiceDate: Date,
-  address: String,
-  district: String,
-  city: String,
-  state: String,
-  cep: String,
-  email: String,
-  contacts: [{ name: String, tel: String, cel: String }],
-  cei: String,
-  gpsCode: String,
-  accessCode: String,
-  accessPassword: String,
-  obs: String,
-  accessoryObligations: [{ name: String, activationDate: Date }]
+	number: Number,
+	alterNumber: Number,
+	name: String,
+	status: String,
+	type: String,
+	cpf: String,
+	specialty: String,
+	startServiceDate: Date,
+	address: String,
+	district: String,
+	city: String,
+	state: String,
+	cep: String,
+	email: String,
+	contacts: [{
+		name: String,
+		tel: String,
+		cel: String
+	}],
+	cei: String,
+	gpsCode: String,
+	accessCode: String,
+	accessPassword: String,
+	municipalRegist: String,
+	crmCro: String,
+	registryOffice: String,
+	obs: String,
+	accessoryObligations: [{
+		name: String,
+		activationDate: Date
+	}]
 });
 
 mongoose.model('Customer', CustomerSchema);
