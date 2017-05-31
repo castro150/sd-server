d34e93b7a8dc2789d45151f455f841e468712dac
castro150 <rafaelcarneiroget@hotmail.com>
2017-05-31 10:07:23 -0300

    Ajuste de describe

--- specs/services/customer.spec.js
+++ specs/services/customer.spec.js
@@ -18,269 +18,277 @@ describe('Customer Service', function() {
 		sandbox.restore();
 	});
 
-	it('create new customer with success', function(done) {
-		var customerProperties = {};
-		customerProperties.name = 'Master Cars';
-		customerProperties.number = 1;
+	describe('create customer', function() {
+		it('create new customer with success', function(done) {
+			var customerProperties = {};
+			customerProperties.name = 'Master Cars';
+			customerProperties.number = 1;
 
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findCustomerStub = sandbox.stub(Customer, 'find').returns({
-			exec: sandbox.stub().yields(null, [])
-		});
-		sandbox.stub(Customer.prototype, 'save').yields(null);
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findCustomerStub = sandbox.stub(Customer, 'find').returns({
+				exec: sandbox.stub().yields(null, [])
+			});
+			sandbox.stub(Customer.prototype, 'save').yields(null);
 
-		CustomerService.create(customerProperties, function(err, savedCustomer) {
-			assert(findCustomerStub.calledWith({
-				number: customerProperties.number,
-				status: CustomerStatus.ACTIVE
-			}));
-			assert(loggerStub.calledWith('New customer created: ' + customerProperties.number + ' - ' + customerProperties.name));
+			CustomerService.create(customerProperties, function(err, savedCustomer) {
+				assert(findCustomerStub.calledWith({
+					number: customerProperties.number,
+					status: CustomerStatus.ACTIVE
+				}));
+				assert(loggerStub.calledWith('New customer created: ' + customerProperties.number + ' - ' + customerProperties.name));
 
-			assert.isNotNull(savedCustomer);
-			assert.equal(savedCustomer.number, customerProperties.number);
-			assert.equal(savedCustomer.name, customerProperties.name);
+				assert.isNotNull(savedCustomer);
+				assert.equal(savedCustomer.number, customerProperties.number);
+				assert.equal(savedCustomer.name, customerProperties.name);
 
-			done();
-		});
-	});
-
-	it('error creating customer with existing number', function(done) {
-		var customerProperties = {};
-		customerProperties.name = 'Master Cars';
-		customerProperties.number = 1;
-
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findCustomerStub = sandbox.stub(Customer, 'find').returns({
-			exec: sandbox.stub().yields(null, [{}])
+				done();
+			});
 		});
 
-		CustomerService.create(customerProperties, function(err, savedCustomer) {
-			assert(findCustomerStub.calledWith({
-				number: customerProperties.number,
-				status: CustomerStatus.ACTIVE
-			}));
-			assert.isFalse(loggerStub.called);
+		it('error creating customer with existing number', function(done) {
+			var customerProperties = {};
+			customerProperties.name = 'Master Cars';
+			customerProperties.number = 1;
 
-			assert.isUndefined(savedCustomer);
-			assert.isNotNull(err);
-			assert.equal(err.name, 'customer.create.active.number.exists');
-			assert.equal(err.status, 400);
-			assert.equal(err.message, 'Create Customer: number ' + customerProperties.number + ' in use for active customer');
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findCustomerStub = sandbox.stub(Customer, 'find').returns({
+				exec: sandbox.stub().yields(null, [{}])
+			});
 
-			done();
-		});
-	});
+			CustomerService.create(customerProperties, function(err, savedCustomer) {
+				assert(findCustomerStub.calledWith({
+					number: customerProperties.number,
+					status: CustomerStatus.ACTIVE
+				}));
+				assert.isFalse(loggerStub.called);
 
-	it('error to execute query while creating customer', function(done) {
-		var queryError = 'query error'
-		var customerProperties = {};
-		customerProperties.name = 'Master Cars';
-		customerProperties.number = 1;
+				assert.isUndefined(savedCustomer);
+				assert.isNotNull(err);
+				assert.equal(err.name, 'customer.create.active.number.exists');
+				assert.equal(err.status, 400);
+				assert.equal(err.message, 'Create Customer: number ' + customerProperties.number + ' in use for active customer');
 
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findCustomerStub = sandbox.stub(Customer, 'find').returns({
-			exec: sandbox.stub().yields(queryError)
-		});
-
-		CustomerService.create(customerProperties, function(err, savedCustomer) {
-			assert(findCustomerStub.calledWith({
-				number: customerProperties.number,
-				status: CustomerStatus.ACTIVE
-			}));
-			assert.isFalse(loggerStub.called);
-
-			assert.isUndefined(savedCustomer);
-			assert.isNotNull(err);
-			assert.equal(err, queryError);
-
-			done();
-		});
-	});
-
-	it('error to save customer while creating customer', function(done) {
-		var saveError = 'save error'
-		var customerProperties = {};
-		customerProperties.name = 'Master Cars';
-		customerProperties.number = 1;
-
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findCustomerStub = sandbox.stub(Customer, 'find').returns({
-			exec: sandbox.stub().yields(null, [])
-		});
-		sandbox.stub(Customer.prototype, 'save').yields(saveError);
-
-		CustomerService.create(customerProperties, function(err, savedCustomer) {
-			assert(findCustomerStub.calledWith({
-				number: customerProperties.number,
-				status: CustomerStatus.ACTIVE
-			}));
-			assert.isFalse(loggerStub.called);
-
-			assert.isUndefined(savedCustomer);
-			assert.isNotNull(err);
-			assert.equal(err, saveError);
-
-			done();
-		});
-	});
-
-	it('find all active customers with success', function(done) {
-		var validCustomer = {};
-		validCustomer.name = 'Master Cars';
-		validCustomer.number = 1;
-
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findCustomerStub = sandbox.stub(Customer, 'find').returns({
-			sort: function() {
-				return this;
-			},
-			select: function() {
-				return this;
-			},
-			exec: sandbox.stub().yields(null, [validCustomer])
+				done();
+			});
 		});
 
-		CustomerService.findAllActive(function(err, customers) {
-			assert(findCustomerStub.calledWith({
-				status: CustomerStatus.ACTIVE
-			}));
-			assert(loggerStub.calledWith('Finding all active customers'));
-			assert(loggerStub.calledWith('1 active customers found'));
+		it('error to execute query while creating customer', function(done) {
+			var queryError = 'query error'
+			var customerProperties = {};
+			customerProperties.name = 'Master Cars';
+			customerProperties.number = 1;
 
-			assert.isNotNull(customers);
-			assert.equal(customers.length, 1);
-			assert.equal(customers[0].number, validCustomer.number);
-			assert.equal(customers[0].name, validCustomer.name);
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findCustomerStub = sandbox.stub(Customer, 'find').returns({
+				exec: sandbox.stub().yields(queryError)
+			});
 
-			done();
+			CustomerService.create(customerProperties, function(err, savedCustomer) {
+				assert(findCustomerStub.calledWith({
+					number: customerProperties.number,
+					status: CustomerStatus.ACTIVE
+				}));
+				assert.isFalse(loggerStub.called);
+
+				assert.isUndefined(savedCustomer);
+				assert.isNotNull(err);
+				assert.equal(err, queryError);
+
+				done();
+			});
+		});
+
+		it('error to save customer while creating customer', function(done) {
+			var saveError = 'save error'
+			var customerProperties = {};
+			customerProperties.name = 'Master Cars';
+			customerProperties.number = 1;
+
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findCustomerStub = sandbox.stub(Customer, 'find').returns({
+				exec: sandbox.stub().yields(null, [])
+			});
+			sandbox.stub(Customer.prototype, 'save').yields(saveError);
+
+			CustomerService.create(customerProperties, function(err, savedCustomer) {
+				assert(findCustomerStub.calledWith({
+					number: customerProperties.number,
+					status: CustomerStatus.ACTIVE
+				}));
+				assert.isFalse(loggerStub.called);
+
+				assert.isUndefined(savedCustomer);
+				assert.isNotNull(err);
+				assert.equal(err, saveError);
+
+				done();
+			});
 		});
 	});
 
-	it('error to execute query while finding all active customers', function(done) {
-		var queryError = 'query error'
-		var validCustomer = {};
-		validCustomer.name = 'Master Cars';
-		validCustomer.number = 1;
+	describe('find all active customers', function() {
+		it('find all active customers with success', function(done) {
+			var validCustomer = {};
+			validCustomer.name = 'Master Cars';
+			validCustomer.number = 1;
 
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findCustomerStub = sandbox.stub(Customer, 'find').returns({
-			sort: function() {
-				return this;
-			},
-			select: function() {
-				return this;
-			},
-			exec: sandbox.stub().yields(queryError)
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findCustomerStub = sandbox.stub(Customer, 'find').returns({
+				sort: function() {
+					return this;
+				},
+				select: function() {
+					return this;
+				},
+				exec: sandbox.stub().yields(null, [validCustomer])
+			});
+
+			CustomerService.findAllActive(function(err, customers) {
+				assert(findCustomerStub.calledWith({
+					status: CustomerStatus.ACTIVE
+				}));
+				assert(loggerStub.calledWith('Finding all active customers'));
+				assert(loggerStub.calledWith('1 active customers found'));
+
+				assert.isNotNull(customers);
+				assert.equal(customers.length, 1);
+				assert.equal(customers[0].number, validCustomer.number);
+				assert.equal(customers[0].name, validCustomer.name);
+
+				done();
+			});
 		});
 
-		CustomerService.findAllActive(function(err, customers) {
-			assert(findCustomerStub.calledWith({
-				status: CustomerStatus.ACTIVE
-			}));
-			assert(loggerStub.calledWith('Finding all active customers'));
+		it('error to execute query while finding all active customers', function(done) {
+			var queryError = 'query error'
+			var validCustomer = {};
+			validCustomer.name = 'Master Cars';
+			validCustomer.number = 1;
 
-			assert.isUndefined(customers);
-			assert.equal(err, queryError);
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findCustomerStub = sandbox.stub(Customer, 'find').returns({
+				sort: function() {
+					return this;
+				},
+				select: function() {
+					return this;
+				},
+				exec: sandbox.stub().yields(queryError)
+			});
 
-			done();
-		});
-	});
+			CustomerService.findAllActive(function(err, customers) {
+				assert(findCustomerStub.calledWith({
+					status: CustomerStatus.ACTIVE
+				}));
+				assert(loggerStub.calledWith('Finding all active customers'));
 
-	it('find customer by id with success', function(done) {
-		var id = '1234567';
-		var validCustomer = {};
-		validCustomer.name = 'Master Cars';
-		validCustomer.number = 1;
+				assert.isUndefined(customers);
+				assert.equal(err, queryError);
 
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findByIdStub = sandbox.stub(Customer, 'findById').returns({
-			exec: sandbox.stub().yields(null, validCustomer)
-		});
-
-		CustomerService.findById(id, function(err, customer) {
-			assert(findByIdStub.calledWith(id));
-			assert(loggerStub.calledWith('User found for id ' + id));
-
-			assert.isNotNull(customer);
-			assert.equal(customer.number, validCustomer.number);
-			assert.equal(customer.name, validCustomer.name);
-
-			done();
-		});
-	});
-
-	it('customer not found while finding by id', function(done) {
-		var id = '1234567';
-
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findByIdStub = sandbox.stub(Customer, 'findById').returns({
-			exec: sandbox.stub().yields(null)
-		});
-
-		CustomerService.findById(id, function(err, customer) {
-			assert(findByIdStub.calledWith(id));
-			assert(loggerStub.calledWith('User not found for id ' + id));
-
-			assert.isUndefined(customer);
-
-			done();
+				done();
+			});
 		});
 	});
 
-	it('error to execute query while finding by id', function(done) {
-		var id = '1234567';
-		var queryError = 'query error'
+	describe('find customer by id', function() {
+		it('find customer by id with success', function(done) {
+			var id = '1234567';
+			var validCustomer = {};
+			validCustomer.name = 'Master Cars';
+			validCustomer.number = 1;
 
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var findByIdStub = sandbox.stub(Customer, 'findById').returns({
-			exec: sandbox.stub().yields(queryError)
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findByIdStub = sandbox.stub(Customer, 'findById').returns({
+				exec: sandbox.stub().yields(null, validCustomer)
+			});
+
+			CustomerService.findById(id, function(err, customer) {
+				assert(findByIdStub.calledWith(id));
+				assert(loggerStub.calledWith('User found for id ' + id));
+
+				assert.isNotNull(customer);
+				assert.equal(customer.number, validCustomer.number);
+				assert.equal(customer.name, validCustomer.name);
+
+				done();
+			});
 		});
 
-		CustomerService.findById(id, function(err, customers) {
-			assert(findByIdStub.calledWith(id));
-			assert(loggerStub.calledWith('Finding customer with id ' + id));
+		it('customer not found while finding by id', function(done) {
+			var id = '1234567';
 
-			assert.isUndefined(customers);
-			assert.equal(err, queryError);
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findByIdStub = sandbox.stub(Customer, 'findById').returns({
+				exec: sandbox.stub().yields(null)
+			});
 
-			done();
+			CustomerService.findById(id, function(err, customer) {
+				assert(findByIdStub.calledWith(id));
+				assert(loggerStub.calledWith('User not found for id ' + id));
+
+				assert.isUndefined(customer);
+
+				done();
+			});
+		});
+
+		it('error to execute query while finding by id', function(done) {
+			var id = '1234567';
+			var queryError = 'query error'
+
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var findByIdStub = sandbox.stub(Customer, 'findById').returns({
+				exec: sandbox.stub().yields(queryError)
+			});
+
+			CustomerService.findById(id, function(err, customers) {
+				assert(findByIdStub.calledWith(id));
+				assert(loggerStub.calledWith('Finding customer with id ' + id));
+
+				assert.isUndefined(customers);
+				assert.equal(err, queryError);
+
+				done();
+			});
 		});
 	});
 
-	it('update customer with success', function(done) {
-		var id = '1234567';
-		var validCustomer = {};
-		validCustomer.name = 'Master Cars';
-		validCustomer.number = 1;
+	describe('update customer', function() {
+		it('update customer with success', function(done) {
+			var id = '1234567';
+			var validCustomer = {};
+			validCustomer.name = 'Master Cars';
+			validCustomer.number = 1;
 
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var updateCustomerStub = sandbox.stub(Customer, 'findByIdAndUpdate').yields(null, validCustomer);
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var updateCustomerStub = sandbox.stub(Customer, 'findByIdAndUpdate').yields(null, validCustomer);
 
-		CustomerService.update(id, validCustomer, function(err, updatedCustomer) {
-			assert(loggerStub.calledWith('Updating customer with id ' + id));
+			CustomerService.update(id, validCustomer, function(err, updatedCustomer) {
+				assert(loggerStub.calledWith('Updating customer with id ' + id));
 
-			assert.isNotNull(updatedCustomer);
-			assert.equal(updatedCustomer.number, validCustomer.number);
-			assert.equal(updatedCustomer.name, validCustomer.name);
+				assert.isNotNull(updatedCustomer);
+				assert.equal(updatedCustomer.number, validCustomer.number);
+				assert.equal(updatedCustomer.name, validCustomer.name);
 
-			done();
+				done();
+			});
 		});
-	});
 
-	it('error to execute query while updating customer', function(done) {
-		var id = '1234567';
-		var queryError = 'query error'
+		it('error to execute query while updating customer', function(done) {
+			var id = '1234567';
+			var queryError = 'query error'
 
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var updateCustomerStub = sandbox.stub(Customer, 'findByIdAndUpdate').yields(queryError);
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var updateCustomerStub = sandbox.stub(Customer, 'findByIdAndUpdate').yields(queryError);
 
-		CustomerService.update(id, {}, function(err, updatedCustomer) {
-			assert(loggerStub.calledWith('Updating customer with id ' + id));
+			CustomerService.update(id, {}, function(err, updatedCustomer) {
+				assert(loggerStub.calledWith('Updating customer with id ' + id));
 
-			assert.isUndefined(updatedCustomer);
-			assert.equal(err, queryError);
+				assert.isUndefined(updatedCustomer);
+				assert.equal(err, queryError);
 
-			done();
+				done();
+			});
 		});
 	});
 });
--- specs/services/security.spec.js
+++ specs/services/security.spec.js
@@ -18,109 +18,113 @@ describe('Security Service', function() {
 		sandbox.restore();
 	});
 
-	it('register user with success', function(done) {
-		var username = 'user';
-		var password = 'pwd';
+	describe('register user', function() {
+		it('register user with success', function(done) {
+			var username = 'user';
+			var password = 'pwd';
 
-		var loggerStub = sandbox.stub(logger, 'debug');
-		sandbox.stub(User.prototype, 'save').yields(null);
+			var loggerStub = sandbox.stub(logger, 'debug');
+			sandbox.stub(User.prototype, 'save').yields(null);
 
-		SecurityService.register(username, password, function(err, user) {
-			assert(loggerStub.calledWith('New user registered: ' + username));
+			SecurityService.register(username, password, function(err, user) {
+				assert(loggerStub.calledWith('New user registered: ' + username));
 
-			assert.isNull(err);
-			assert.isNotNull(user);
-			assert.equal(user.username, username);
-			assert.isNotNull(user.salt);
-			assert.isNotNull(user.hash);
+				assert.isNull(err);
+				assert.isNotNull(user);
+				assert.equal(user.username, username);
+				assert.isNotNull(user.salt);
+				assert.isNotNull(user.hash);
 
-			done();
+				done();
+			});
+		});
+
+		it('error while registering existing user', function(done) {
+			var username = 'user';
+			var password = 'pwd';
+
+			sandbox.stub(User.prototype, 'save').yields({
+				code: 11000
+			});
+
+			SecurityService.register(username, password, function(err, user) {
+				assert.isUndefined(user);
+				assert.isNotNull(err);
+				assert.equal(err.code, 11000);
+				assert.equal(err.name, 'security.register.user.already.exists');
+
+				done();
+			});
+		});
+
+		it('generic error while registering user', function(done) {
+			var username = 'user';
+			var password = 'pwd';
+
+			sandbox.stub(User.prototype, 'save').yields({
+				name: 'generic.error',
+				code: 500
+			});
+
+			SecurityService.register(username, password, function(err, user) {
+				assert.isUndefined(user);
+				assert.isNotNull(err);
+				assert.equal(err.code, 500);
+				assert.equal(err.name, 'generic.error');
+
+				done();
+			});
 		});
 	});
 
-	it('error while registering existing user', function(done) {
-		var username = 'user';
-		var password = 'pwd';
+	describe('renew token', function() {
+		it('renew token with success', function(done) {
+			var clock = sinon.useFakeTimers();
 
-		sandbox.stub(User.prototype, 'save').yields({
-			code: 11000
+			var oldToken = '1234567';
+			var renewedToken = '7654321';
+			var user = {};
+			user.username = 'user';
+			user.password = 'pwd';
+
+			var loggerStub = sandbox.stub(logger, 'debug');
+			var signStub = sandbox.stub(jwtoken, 'sign').returns(renewedToken);
+			sandbox.stub(jwtoken, 'verify').yields(null, user);
+
+			SecurityService.renewToken(oldToken, function(err, newToken) {
+				assert(loggerStub.calledWith('New token for user: ' + user.username));
+
+				var capturedUser = signStub.getCall(0).args[0];
+				var expectedExp = new Date();
+				expectedExp.setMinutes(new Date().getMinutes() + 1000);
+
+				assert.equal(capturedUser.username, user.username);
+				assert.equal(capturedUser.password, user.password);
+				assert.equal(capturedUser.exp, parseInt(expectedExp.getTime() / 1000));
+
+				assert.isNull(err);
+				assert.equal(newToken, renewedToken);
+
+				clock.restore();
+				done();
+			});
 		});
 
-		SecurityService.register(username, password, function(err, user) {
-			assert.isUndefined(user);
-			assert.isNotNull(err);
-			assert.equal(err.code, 11000);
-			assert.equal(err.name, 'security.register.user.already.exists');
+		it('sign error while trying to renew token', function(done) {
+			var oldToken = '1234567';
+			var signError = 'error to sign';
 
-			done();
-		});
-	});
+			var loggerStub = sandbox.stub(logger, 'debug');
+			sandbox.stub(jwtoken, 'verify').yields(signError);
 
-	it('generic error while registering user', function(done) {
-		var username = 'user';
-		var password = 'pwd';
+			SecurityService.renewToken(oldToken, function(err, newToken) {
+				assert.isFalse(loggerStub.called);
 
-		sandbox.stub(User.prototype, 'save').yields({
-			name: 'generic.error',
-			code: 500
-		});
+				assert.equal(err, signError);
+				assert.isUndefined(newToken);
 
-		SecurityService.register(username, password, function(err, user) {
-			assert.isUndefined(user);
-			assert.isNotNull(err);
-			assert.equal(err.code, 500);
-			assert.equal(err.name, 'generic.error');
-
-			done();
-		});
-	});
-
-	it('renew token with success', function(done) {
-		var clock = sinon.useFakeTimers();
-
-		var oldToken = '1234567';
-		var renewedToken = '7654321';
-		var user = {};
-		user.username = 'user';
-		user.password = 'pwd';
-
-		var loggerStub = sandbox.stub(logger, 'debug');
-		var signStub = sandbox.stub(jwtoken, 'sign').returns(renewedToken);
-		sandbox.stub(jwtoken, 'verify').yields(null, user);
-
-		SecurityService.renewToken(oldToken, function(err, newToken) {
-			assert(loggerStub.calledWith('New token for user: ' + user.username));
-
-			var capturedUser = signStub.getCall(0).args[0];
-			var expectedExp = new Date();
-			expectedExp.setMinutes(new Date().getMinutes() + 1000);
-
-			assert.equal(capturedUser.username, user.username);
-			assert.equal(capturedUser.password, user.password);
-			assert.equal(capturedUser.exp, parseInt(expectedExp.getTime() / 1000));
-
-			assert.isNull(err);
-			assert.equal(newToken, renewedToken);
-
-			clock.restore();
-			done();
-		});
-	});
-
-	it('sign error while trying to renew token', function(done) {
-		var oldToken = '1234567';
-		var signError = 'error to sing';
-
-		var loggerStub = sandbox.stub(logger, 'debug');
-		sandbox.stub(jwtoken, 'verify').yields(signError);
-
-		SecurityService.renewToken(oldToken, function(err, newToken) {
-			assert.isFalse(loggerStub.called);
-
-			assert.equal(err, signError);
-			assert.isUndefined(newToken);
-
-			done();
+				done();
+			});
 		});
 	});
 });
